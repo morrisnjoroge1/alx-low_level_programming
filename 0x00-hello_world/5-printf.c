873e9248871b136fcc3c9cc66927ae930accc633
morris <mnjoroge25439@gmail.com>
2023-06-15 07:18:18 -0700

    commit update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  */
 #include <stdio.h>
 
-int main() {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
 }
-
