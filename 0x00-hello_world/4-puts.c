aaf645a332e895c0d52688444b5e11f1b01d77a4
morris <mnjoroge25439@gmail.com>
2023-06-15 09:04:27 -0700

    commit updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	printf("printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
     }
 
