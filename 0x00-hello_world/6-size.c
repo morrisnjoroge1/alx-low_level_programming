316ca8eec20dc612d3ccce31868e7a18c74f6490
morris <mnjoroge25439@gmail.com>
2023-06-15 12:46:22 -0700

    commit update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,11 @@
  */
 int main(void)
 {
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longlongIntType;
+	float floatType;
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of a int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
