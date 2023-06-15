74259ce3bf8d66b87d44f3cd95a4073e98d3d65f
morris <mnjoroge25439@gmail.com>
2023-06-15 09:48:43 -0700

    commit update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,6 @@
  * main - A program that print a line using the print function
  * Return: 0 (Success)
  */
-#include <stdio.h>
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
