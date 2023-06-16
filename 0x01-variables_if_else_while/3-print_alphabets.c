12f4d301aa80765924cf9c9aea73395a95c00075
morris <mnjoroge25439@gmail.com>
2023-06-16 13:40:42 -0700

    commit update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succession)
+ */
+int main(void)
+{
+	int l = 97;
+	int p = 65;
+
+	while (l <= 122)
+	{
+		putchar(l);
+	}
+	while (p <= 90)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
