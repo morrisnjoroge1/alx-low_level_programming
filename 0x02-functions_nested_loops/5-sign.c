35c2f321e13d880289fd1260bb2f455e166ccc88
morris <mnjoroge25439@gmail.com>
2023-06-20 12:24:57 -0700

    commit update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,16 +5,24 @@
  *
  * @c: The character in ASCII code
  *
- * Return: 1 for letters. 0 for the rest.
+ * Return: 1 is greater than zero. 0 is zero'
+ * -1 is less than zero
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
+		_putchar(48);
 		return (0);
 	}
 	_putchar('\n');
