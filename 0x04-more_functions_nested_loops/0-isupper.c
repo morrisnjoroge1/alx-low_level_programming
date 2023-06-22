44bdec698a96a4d9530130d991c953181681b765
morris <mnjoroge25439@gmail.com>
2023-06-22 10:59:53 -0700

    commit update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * -isuper - checks for uppercase character
+ *  @c: number to be checked
+ *  Return 1 (for uppercase), 0 for other 
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <=90)
+	{
+		return (1);
+	}
+	return (0);
+}
+
