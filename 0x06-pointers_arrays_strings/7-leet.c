8b92ab135058f0252f1230c78373f7655b750e8e
morris <mnjoroge25439@gmail.com>
2023-06-29 13:11:05 -0700

    update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - check the code for Holberton School students.
+ *@n : variable
+ * Return: return
+ */
+char *leet(char *n)
+{
+int i;
+int a;
+char b[11] = "aAeEoOtTlL";
+char c[11] = "4433007711";
+
+for (i = 0; n[i] != '\0'; i++)
+{
+for (a = 0; a <= 9; a++)
+{
+if (b[a] == n[i])
+{
+n[i] = c[a];
+}
+}
+}
+return (n);
+}
