414753c9fc4f516debc0b4aac51b5e83c280491b
morris <mnjoroge25439@gmail.com>
2023-06-30 13:23:38 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,32 +6,19 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	unsigned int n1;
+
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
-	}
-	int divisor = 1;
-	int temp = n;
-
-	while (temp != 0)
+	} else
 	{
-		divisor *= 10;
-		temp /= 10;
+		n1 = n;
 	}
-	divisor /= 10;
 
-	while (divisor != 0)
-	{
-		int digit = n / divisor;
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-		_putchar('0' + digit);
-		n %= divisor;
-		divisor /= 10;
-	}
+	_putchar((n1 % 10) + '0');
 }
