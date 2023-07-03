8792724afd1917fc723948f54d3abc396c7dfa78
morris <mnjoroge25439@gmail.com>
2023-07-03 12:01:48 -0700

    square matrix of integers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 /**
  * print_diagsums - sum of the two diagonals of a square matrix of integer
- * @a: pointer
- * @i: variable
- * @p:
+ * @a: pointer array
+ * @i: array
+ * @size: array
  * @s
  */
 void print_diagsums(int *a, int size)
 {
-	int psums = 0;
-	int ssums = 0;
 	int i;
+	int j;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		psums += a[i * size + i];
-		ssums += a[i * size + (size - 1 - i)];
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 += a[i * size + j];
+			}
+			if (i + j == size - 1)
+			{
+				sum2 += a[i * size + j];
+			}
+		}
 	}
-	printf("%d\n", psums);
-	printf("%d\n", ssums);
+	printf("%d, %d\n", sum1, sum2);
 }
