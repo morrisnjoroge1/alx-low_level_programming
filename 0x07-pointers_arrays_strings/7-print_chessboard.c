7589ed24c66c1294589bcf7eb6199afdf20f0a27
morris <mnjoroge25439@gmail.com>
2023-07-03 10:49:44 -0700

    chaseboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard -  prints the chessboard
+ * @a: pointer of arrays
+ * @i:
+ * @j:
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
