9b64f75d62ba9dce3586957ca86ab52d6cfcb14c
morris <mnjoroge25439@gmail.com>
2023-07-05 12:23:22 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@
 int is_prime_helper(int n, int divisor);
 int is_prime_number(int n)
 {
-	if (n < 1)
+	if (n <= 1)
 		return (0);
 	return (is_prime_helper(n, 2));
 }
@@ -25,7 +25,7 @@ int is_prime_helper(int n, int divisor)
 		return (1);/*if it is a prime number*/
 	if (n % divisor == 0)
 		return (0); /*if divisible by divisor*/
-	if (divisor == 2)
-		return is_prime_helper(n, divisor + 1); /*for even number*/
+	if (divisor == 2 || divisor == 3)
+		return (is_prime_helper(n, divisor + 1)); /*for even number*/
 	return (is_prime_helper(n, divisor + 1));/*odd number only*/
 }
