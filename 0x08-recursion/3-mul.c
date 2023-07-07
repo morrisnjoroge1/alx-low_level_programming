9caea2882f5cccde4514ca57e4fd954cc283b0d8
morris <mnjoroge25439@gmail.com>
2023-07-07 04:52:43 -0700

    multiplies

--- 0x08-recursion/3-mul.c
+++ 0x08-recursion/3-mul.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * main -  program that multiplies two numbers input in
- * @argc: input
- * @argv: array
- * Return: Error or 1
- */
+* main -  program that multiplies two numbers input in
+* @argc: input
+* @argv: array
+* Return: Error or 1
+*/
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-	printf("%d\n", result);
-	return (0);
+int m;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+m = (atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", m);
+}
+return (0);
 }
