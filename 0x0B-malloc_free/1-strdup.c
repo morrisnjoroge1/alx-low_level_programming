1d7ce6e1654d45644b5d38cc594d2954c91b4d9c
morris <mnjoroge25439@gmail.com>
2023-07-10 13:13:15 -0700

    pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,28 +12,25 @@
 */
 char *_strdup(char *str)
 {
-int i = 0;
-int l =0 ;
-char *d;
+int i = 0, l = 0;
+	char *s;
 
-if (str == NULL)
-{
-return (NULL);
-}
-/*length of string*/
-while (str[l] != '\0')
-l++;
-/*memory for duplicate str*/
-d = malloc((1 + 1) * (sizeof(char)));
-if (d == NULL)
-{
-return (NULL);
-}
-while (str[i] != '\0')
-/*copying form *str to *d*/
-{
-d[i] = str[i];
-i++;
-}
-return (d);
+	if (str == NULL)
+		return (NULL);
+
+	/*calculate size of str*/
+	while (str[l] != '\0')
+		l++;
+
+	s = malloc((l + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	return (s);
 }
