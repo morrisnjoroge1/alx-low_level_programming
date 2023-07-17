214346152d8e8fc47867e3842c432babbb107fd5
morris <mnjoroge25439@gmail.com>
2023-07-17 08:56:39 -0700

    initialize a variable

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,6 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t -Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
