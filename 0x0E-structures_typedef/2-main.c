06dee11191d123555e2f3e3a5ba9e5cf92da153b
morris <mnjoroge25439@gmail.com>
2023-07-17 09:08:51 -0700

    prints a struct dog

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -6,12 +6,15 @@
  *
  * Return: Always 0.
 */
+void print_dog(const struct dog* d);
 
 int main(void)
 {
 	struct dog my_dog;
 
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
 	return (0);
 }
