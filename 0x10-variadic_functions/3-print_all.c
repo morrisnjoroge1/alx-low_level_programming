86ecdf4422191843274da82d68f71fb3571b5fc3
morris <mnjoroge25439@gmail.com>
2023-07-20 11:55:39 -0700

    variadic

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints all of the arguments when specified
+ * @format: specifies the necessary operations
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	int flag;
+	char *str;
+	va_list a_list;
+
+	 va_start(args, format);
