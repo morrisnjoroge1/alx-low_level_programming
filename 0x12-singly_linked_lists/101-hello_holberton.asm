6afeea9270b4bb42861e34392a7fb749f33491d8
morris <mnjoroge25439@gmail.com>
2023-07-27 21:59:35 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,15 @@
 SECTION .data
-msg:	db "Hello, Holberton", 0
+msg:	db "Hello, Holberton", 0	; string termination(null)
 fmt:	db "%s", 10, 0
 
-	SECTION .text
-	extern printf
+Section .text
+extern printf	;from the standard library
 	global main
 main:
 	mov esi, msg
 	mov edi, fmt
 	mov eax, 0
+	push rbp
 	call printf
 
 	mov eax, 0
