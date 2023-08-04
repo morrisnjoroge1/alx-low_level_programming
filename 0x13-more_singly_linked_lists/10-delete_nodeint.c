d2b9708c83087a2f3f1a22aebbc8332054f98e4e
morris <mnjoroge25439@gmail.com>
2023-08-04 11:12:53 -0700

    more linked

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes the node at index index of a linked list
  * @head: pointer to node
  * @index: nth index of node
  *
