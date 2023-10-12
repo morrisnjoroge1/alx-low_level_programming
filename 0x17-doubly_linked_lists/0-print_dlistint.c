f28edc83a650e2e037a3a027ce72135f6e2e5f2c
morris <mnjoroge25439@gmail.com>
2023-10-12 11:19:18 -0700

    linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: header pointer of list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0; /*intiate the count*/
+
+	if ( h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n); /*print current node data*/
+		count++;
+		h = h->next;/*moving to next node*/
+	}
+	return (count);
+}
