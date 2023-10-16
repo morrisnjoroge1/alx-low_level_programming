434f2e7da68c15a86f9e1260c5c4a0f54fc36302
morris <mnjoroge25439@gmail.com>
2023-10-16 12:27:06 -0700

    lib:advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+* add - adds two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the sum
+*/
+int add(int a, int b)
+{
+	return (a+b);
+}
+
+/**
+* sub - subtracts two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the subtract
+*/
+int sub(int a, int b)
+{
+	return (a-b);
+}
+
+/**
+* mul - multiplies two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the multiple
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divideds two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the dividend integer
+*/
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+	return (a / b);
+}
+
+/**
+  mod - finds the modulus of two integers
+  @a: first integer to add
+  @b: second integer to add
+  Return: the modulus
+*/
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: division by zero\n");
+		return (0);
+	}
+	return (a % b);
+}
